8c01a4a38231d46963db5dafd3d408933fc496fe
snd-07 <rect20250725@rect20250725noMacBook-Air.local>
2025-11-25 16:06:42 +0900

    パンクズ修正

--- src/lib/const/BreadCrumbTitle.tsx
+++ src/lib/const/BreadCrumbTitle.tsx
@@ -2,5 +2,5 @@ export const schoolTopTitle = ['プログラミング教室 Re:ProS'];
 export const demandTopTitle = ['開発等の相談'];
 export const fcTopTitle = [
   ...schoolTopTitle,
-  'プログラミング教室 Re:ProS フランチャイズオーナー募集サイト',
+  'こども向けプログラミング教室のフランチャイズパートナー募集 | プログラミング教室 Re:ProS(レプロス repros)',
 ];
