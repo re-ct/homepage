af7b22cd8c6c7572089595df527317ebf968c3b9
snd-07 <rect20250725@rect20250725noMacBook-Air.local>
2025-12-19 12:47:57 +0900

    urlの作成

---
+++ src/app/career/page.tsx
@@ -0,0 +1,16 @@
+import '../globals.css';
+
+export const metadata = {
+  title:
+    'ソフトウェア開発の相談・依頼 | 開発実績多数！ソフトウェア開発の依頼は株式会社レクトへ',
+};
+
+const Demand = () => {
+  return (
+    <main style={{ color: '#000' }}>
+      <p>ほげ</p>
+    </main>
+  );
+};
+
+export default Demand;
