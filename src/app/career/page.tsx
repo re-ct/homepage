8fe6c9cc6b974028ebe827432a63002d8974f2f8
snd-07 <rect20250725@rect20250725noMacBook-Air.local>
2025-12-22 14:39:29 +0900

    titleタグ設定

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -7,6 +7,12 @@ import KeyVisual from './KeyVisual';
 
 export const metadata = {
   title: 'エンジニアのお悩みを無料でサポート Re:ProS Career',
+  description:
+    'キャリアのモヤモヤ、現役エンジニアと話してみませんか？現場を知るプロが、スキル評価や成長ロードマップを個別に提案。転職意欲の有無は問いません。まずは今の悩みをお聞かせください。',
+  robots: 'index,follow',
+  alternates: {
+    canonical: 'https://re-ct.co.jp/career',
+  },
 };
 
 const Demand = () => {
