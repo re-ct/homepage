eee52f37de3e107ffcb89047316031175aa73ff2
snd-07 <rect20250725@rect20250725noMacBook-Air.local>
2025-12-23 12:40:39 +0900

    lint

--- src/lib/const/Link.tsx
+++ src/lib/const/Link.tsx
@@ -1,4 +1,5 @@
 export const contact = 'https://forms.gle/tWbY3pJaxRpEq7Nz8';
 export const fcRequestForDocument = 'https://forms.gle/Gbr5BnxzrVHyZ8Xs7';
 export const fcContact = 'https://forms.gle/UXFbp3GQtaerFasc7';
-export const careerContact = 'https://docs.google.com/forms/d/e/1FAIpQLSdNk4Wh7pRY-zogTsaoQKTwE9H5nj6XMdHTl_hFn2kGdp90gQ/viewform?usp=header';
+export const careerContact =
+  'https://docs.google.com/forms/d/e/1FAIpQLSdNk4Wh7pRY-zogTsaoQKTwE9H5nj6XMdHTl_hFn2kGdp90gQ/viewform?usp=header';
