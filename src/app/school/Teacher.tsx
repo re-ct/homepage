45e1f19f668196ca545e5886968e67b5c0aa4d9d
snd-07 <rect20250725@rect20250725noMacBook-Air.local>
2026-02-10 11:36:33 +0900

    SP調整

--- src/app/school/Teacher.tsx
+++ src/app/school/Teacher.tsx
@@ -40,6 +40,16 @@ const sxStyles = {
       width: '100%',
       height: 'auto',
     },
+    '&:first-child': {
+      '@media screen and (max-width:768px)': {
+        paddingLeft: '24px',
+      },
+    },
+    '&:last-child': {
+      '@media screen and (max-width:768px)': {
+        paddingRight: '24px',
+      },
+    },
   },
   body: {
     textAlign: 'left',
@@ -50,6 +60,11 @@ const sxStyles = {
     textAlign: 'left',
     fontSize: '20px',
   },
+  teacherListWrap: {
+    '@media screen and (max-width:768px)': {
+      overflowX: 'scroll',
+    },
+  },
   teacherList: {
     display: 'grid',
     gridTemplateColumns: '1fr 1fr',
@@ -58,8 +73,8 @@ const sxStyles = {
     marginInline: 'auto',
     mt: 8,
     '@media screen and (max-width:768px)': {
-      gridTemplateColumns: '1fr',
-      rowGap: '56px',
+      gridTemplateColumns: '90% 90%',
+      columnGap: '24px',
     },
   },
   subTitle: {
@@ -67,48 +82,58 @@ const sxStyles = {
     mt: 3,
     textAlign: 'left',
   },
+  section: {
+    pt: 9,
+    pb: 10,
+    mx: 3,
+    '@media screen and (max-width:768px)': {
+      mx: 0,
+    },
+  },
 };
 
 const Teacher = () => {
   return (
-    <Box pt={9} pb={10} mx={3} id="school_teacher">
+    <Box sx={sxStyles.section} id="school_teacher">
       <Typography variant="h2" sx={sxStyles.heading}>
         <img src={headingTeacherImg.src} alt="講師" width="127" height="59" />
       </Typography>
-      <Box component="ul" sx={sxStyles.teacherList}>
-        <Box component="li" sx={sxStyles.teacher}>
-          <img src={watanabe.src} width="500" height="416" />
-          <Typography variant="h3" sx={sxStyles.name}>
-            渡辺 優樹
-          </Typography>
-          <Typography variant="body2" sx={sxStyles.body}>
-            プライム上場のエイチームグループにて5年間Webエンジニアを経験し、5人のメンバーのマネジメントも経験しました。
-          </Typography>
-          <Typography variant="body2" sx={sxStyles.body}>
-            プログラミングを通して、次の世代に残る何かを残したいと思い起業しました！とにかくまずはプログラミングを楽しいと思ってもらえるように頑張ります！
-          </Typography>
-          <Typography sx={sxStyles.subTitle}>経歴・資格について</Typography>
-          <List sx={sxStyles.list}>
-            <ListItem>名古屋大学大学院情報学研究科 修了</ListItem>
-            <ListItem>高等学校教諭一種免許状「情報」 免許保有</ListItem>
-            <ListItem>基本情報技術者 資格保有</ListItem>
-          </List>
-        </Box>
-        <Box component="li" sx={sxStyles.teacher}>
-          <img src={suzuki.src} width="500" height="416" />
-          <Typography variant="h3" sx={sxStyles.name}>
-            鈴木 佑基
-          </Typography>
-          <Typography variant="body2" sx={sxStyles.body}>
-            ネットイーグル株式会社で7年間セールスエンジニアを務め、現在は当スクールの講師をしています。
-          </Typography>
-          <Typography variant="body2" sx={sxStyles.body}>
-            生徒一人ひとりのペースに合わせた丁寧な解説とコミュニケーションで、お子様の「自分で考える力」をプログラミングを通して引き出していきます！
-          </Typography>
-          <Typography sx={sxStyles.subTitle}>経歴・資格について</Typography>
-          <List sx={sxStyles.list}>
-            <ListItem>中部大学情報工学科卒</ListItem>
-          </List>
+      <Box sx={sxStyles.teacherListWrap}>
+        <Box component="ul" sx={sxStyles.teacherList}>
+          <Box component="li" sx={sxStyles.teacher}>
+            <img src={watanabe.src} width="500" height="416" />
+            <Typography variant="h3" sx={sxStyles.name}>
+              渡辺 優樹
+            </Typography>
+            <Typography variant="body2" sx={sxStyles.body}>
+              プライム上場のエイチームグループにて5年間Webエンジニアを経験し、5人のメンバーのマネジメントも経験しました。
+            </Typography>
+            <Typography variant="body2" sx={sxStyles.body}>
+              プログラミングを通して、次の世代に残る何かを残したいと思い起業しました！とにかくまずはプログラミングを楽しいと思ってもらえるように頑張ります！
+            </Typography>
+            <Typography sx={sxStyles.subTitle}>経歴・資格について</Typography>
+            <List sx={sxStyles.list}>
+              <ListItem>名古屋大学大学院情報学研究科 修了</ListItem>
+              <ListItem>高等学校教諭一種免許状「情報」 免許保有</ListItem>
+              <ListItem>基本情報技術者 資格保有</ListItem>
+            </List>
+          </Box>
+          <Box component="li" sx={sxStyles.teacher}>
+            <img src={suzuki.src} width="500" height="416" />
+            <Typography variant="h3" sx={sxStyles.name}>
+              鈴木 佑基
+            </Typography>
+            <Typography variant="body2" sx={sxStyles.body}>
+              ネットイーグル株式会社で7年間セールスエンジニアを務め、現在は当スクールの講師をしています。
+            </Typography>
+            <Typography variant="body2" sx={sxStyles.body}>
+              生徒一人ひとりのペースに合わせた丁寧な解説とコミュニケーションで、お子様の「自分で考える力」をプログラミングを通して引き出していきます！
+            </Typography>
+            <Typography sx={sxStyles.subTitle}>経歴・資格について</Typography>
+            <List sx={sxStyles.list}>
+              <ListItem>中部大学 工学部情報工学科卒</ListItem>
+            </List>
+          </Box>
         </Box>
       </Box>
     </Box>
