c975dbc1c69e576e74f2a495d1717f2b03074107
snd-07 <rect20250725@rect20250725noMacBook-Air.local>
2026-02-16 12:13:29 +0900

    パンクズ調整

--- src/lib/const/BreadCrumbTitle.tsx
+++ src/lib/const/BreadCrumbTitle.tsx
@@ -1,6 +1,6 @@
 export const schoolTopTitle = ['プログラミング教室 Re:ProS'];
 export const demandTopTitle = ['開発等の相談'];
-export const downloadTitle = ['資料一覧'];
+export const downloadTitle = ['お役立ち資料一覧'];
 export const fcTopTitle = [
   ...schoolTopTitle,
   'こども向けプログラミング教室のフランチャイズパートナー募集',
