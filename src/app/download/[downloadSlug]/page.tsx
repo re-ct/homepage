180dc187e9f0417537fd33ddff6d7a5d08a9310c
snd-07 <rect20250725@rect20250725noMacBook-Air.local>
2026-02-16 11:04:57 +0900

    スタイリング

--- src/app/download/[downloadSlug]/page.tsx
+++ src/app/download/[downloadSlug]/page.tsx
@@ -4,6 +4,7 @@ import { Download } from '../../../lib/const/Download';
 import { notFound } from 'next/navigation';
 import BreadcrumbsNavigation from '../../BreadcrumbsNavigation';
 import { downloadTitle } from '@/lib/const/BreadCrumbTitle';
+import Typography from '@mui/material/Typography';
 
 export async function generateStaticParams() {
   return Download.map((course) => ({
@@ -26,19 +27,44 @@ export async function generateMetadata({ params }: DownloadDetailsPageProps) {
   };
 }
 
+const sxStyles = {
+  heading: {
+    fontSize: '38px',
+    fontWeight: 'bold',
+  },
+  text: {
+    fontSize: '14px',
+    mt: '8px',
+  },
+  wrap: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 550px',
+    mt: '72px',
+    '@media screen and (max-width:768px)': {
+      gridTemplateColumns: '1fr',
+      rowGap: '40px',
+    },
+  },
+  name: {
+    fontSize: '24px',
+    fontWeight: 'bold',
+    lineHeight: '1.6',
+  },
+  form: {
+    iframe: {
+      width: '100%',
+      border: 'none',
+    },
+  },
+};
+
 interface DownloadDetailsPageProps {
   params: Promise<{
     downloadSlug: string;
-    name: {
-      category: string;
-      level: string;
-    };
   }>;
 }
 
-const DownloadDetailsPage: React.FC<DownloadDetailsPageProps> = async ({
-  params,
-}: DownloadDetailsPageProps) => {
+const DownloadDetailsPage = async ({ params }: DownloadDetailsPageProps) => {
   const { downloadSlug } = await params;
   const course = Download.find((course) => `${course.slug}` === downloadSlug);
 
@@ -46,19 +72,57 @@ const DownloadDetailsPage: React.FC<DownloadDetailsPageProps> = async ({
     return notFound();
   }
 
-  downloadTitle.push(`${course.name}`);
+  const currentBreadcrumbs = [...downloadTitle, course.name];
 
   return (
     <main>
       <Box
         component="section"
         sx={{
-          textAlign: 'center',
+          maxWidth: '940px',
           mx: 'auto',
+          px: '20px',
+          pt: '70px',
+          pb: '100px',
         }}
       >
-        <BreadcrumbsNavigation titles={downloadTitle} />
+        <Typography variant="h1" sx={sxStyles.heading}>
+          資料のお問い合わせ
+        </Typography>
+        <Typography sx={sxStyles.text}>
+          資料を閲覧したい方は、お申し込みフォームからお問い合わせください。
+        </Typography>
+        <Box sx={sxStyles.wrap}>
+          <Box>
+            <Typography variant="h1" sx={sxStyles.name}>
+              {course.name}
+            </Typography>
+            <img
+              src={`../../../image/download/${course.thumbnail}`}
+              alt=""
+              width="500"
+              height="300"
+              style={{
+                width: '100%',
+                height: 'auto',
+                borderRadius: '8px',
+                marginTop: '20px',
+              }}
+            />
+            <Typography sx={sxStyles.text}>{course.details}</Typography>
+          </Box>
+          <Box sx={sxStyles.form}>
+            <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLSexsVLTHkqbV6XYvG_5X8PBckIWwYf3mXybYD7PKg0YU9IL5A/viewform"
+              width="640"
+              height="1300"
+            >
+              読み込んでいます…
+            </iframe>
+          </Box>
+        </Box>
       </Box>
+      <BreadcrumbsNavigation titles={currentBreadcrumbs} />
     </main>
   );
 };
--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -2,6 +2,10 @@ import '../globals.css';
 import BreadcrumbsNavigation from '../BreadcrumbsNavigation';
 import Box from '@mui/material/Box';
 import { downloadTitle } from '@/lib/const/BreadCrumbTitle';
+import { Download } from '../../lib/const/Download';
+import Typography from '@mui/material/Typography';
+import { CommonLink } from '../../components/CommonLink';
+import { ArrowForward } from '@mui/icons-material';
 
 export const metadata = {
   title: '資料一覧 | 株式会社レクト',
@@ -12,22 +16,121 @@ export const metadata = {
   },
 };
 
-const Download = () => {
+const sxStyles = {
+  heading: {
+    fontSize: '38px',
+    fontWeight: 'bold',
+  },
+  text: {
+    fontSize: '14px',
+    mt: '8px',
+  },
+  box: {
+    mt: '52px',
+    '& + &': {
+      mt: '72px,',
+    },
+  },
+  category: {
+    fontSize: '24px',
+    fontWeight: 'bold',
+  },
+  list: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3,1fr)',
+    listStyle: 'none',
+    gap: '62px 10px',
+    mt: 3,
+    '@media screen and (max-width:768px)': {
+      gridTemplateColumns: '1fr',
+    },
+    '>li >a': {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+  },
+  button: {
+    color: '#00298A',
+    border: 'solid 1px #00298A',
+    borderRadius: '50px',
+    padding: '8px 20px',
+    width: 'fit-content',
+    fontSize: '12px',
+    fontWeight: 'bold',
+    mt: '18px',
+    columnsGap: '8px',
+    display: 'flex',
+    alignItems: 'center',
+    '.arrow-icon': {
+      fontSize: '14px',
+    },
+  },
+  title: {
+    fontSize: '16px',
+    fontWeight: 'bold',
+    mt: 2,
+  },
+};
+
+const DownloadPage = () => {
   return (
     <main>
       <Box
         component="section"
         sx={{
-          textAlign: 'center',
+          maxWidth: '940px',
           mx: 'auto',
+          px: '20px',
+          pt: '70px',
+          pb: '100px',
         }}
       >
-        <h1>資料一覧</h1>
-        <p>弊社のサービスや活動に関する資料をご覧いただけます。</p>
-        <BreadcrumbsNavigation titles={downloadTitle} />
+        <Typography variant="h1" sx={sxStyles.heading}>
+          資料一覧
+        </Typography>
+        <Typography sx={sxStyles.text}>
+          弊社のサービスや活動に関する資料をご覧いただけます。
+        </Typography>
+        <Box component="section" sx={sxStyles.box}>
+          <Typography variant="h2" sx={sxStyles.category}>
+            ノウハウ
+          </Typography>
+          <Box component="ul" sx={sxStyles.list}>
+            {Download.map((item) => (
+              <Box component="li" key={item.slug}>
+                <CommonLink
+                  link={{
+                    href: `/download/${item.slug}`,
+                    external: false,
+                  }}
+                >
+                  <img
+                    src={`../../../image/download/${item.thumbnail}`}
+                    alt=""
+                    width="500"
+                    height="300"
+                    style={{
+                      width: '100%',
+                      height: 'auto',
+                      borderRadius: '8px',
+                    }}
+                  />
+                  <Typography component="h3" sx={sxStyles.title}>
+                    {item.name}
+                  </Typography>
+                  <Typography sx={sxStyles.button}>
+                    ダウンロード
+                    <ArrowForward className="arrow-icon" />
+                  </Typography>
+                </CommonLink>
+              </Box>
+            ))}
+          </Box>
+        </Box>
       </Box>
+      <BreadcrumbsNavigation titles={downloadTitle} />
     </main>
   );
 };
 
-export default Download;
+export default DownloadPage;
