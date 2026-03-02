24b6abd9d79017d0e9ddb7db7d8e19ab95622402
snd-07 <rect20250725@rect20250725noMacBook-Air.local>
2026-03-02 10:27:13 +0900

    0件の対応リバート

--- src/app/announcement/Post.tsx
+++ src/app/announcement/Post.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Typography, Box } from '@mui/material';
+import { Box } from '@mui/material';
 import { Announcement } from '../../lib/const/Announcement';
 import { CommonLink } from '../../components/CommonLink';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const sxStyles = {
-  noData: {
-    pt: 8,
-    color: '#545454',
-  },
   ul: {
     marginTop: '32px',
     listStyle: 'none',
@@ -78,47 +74,11 @@ const list = Announcement.map((post) => (
 ));
 
 const Post = () => {
-  const hasPosts = Announcement.length > 0;
   return (
     <>
-      {hasPosts ? (
-        <Box component="ul" sx={sxStyles.ul}>
-          {list}
-        </Box>
-      ) : (
-        <>
-          <Typography sx={sxStyles.noData}>
-            現在、掲載すべき電子公告はありません。
-          </Typography>
-          <CommonLink
-            link={{
-              href: `/`,
-              external: false,
-            }}
-            style={{
-              backgroundColor: '#00298A',
-              textTransform: 'none',
-              padding: '16px 20px 16px 20px',
-              borderRadius: '50px',
-              boxShadow: 'none',
-              minWidth: '283px',
-              width: 'fit-content',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              columnGap: '10px',
-              transition: 'filter 0.3s',
-              textDecoration: 'none',
-              color: '#fff',
-              fontWeight: 'bold',
-              marginTop: '36px',
-            }}
-          >
-            サイトトップへ戻る
-            <ArrowForwardIcon />
-          </CommonLink>
-        </>
-      )}
+      <Box component="ul" sx={sxStyles.ul}>
+        {list}
+      </Box>
     </>
   );
 };
