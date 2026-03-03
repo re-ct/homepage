6cec158394b75c70a8d324fc5e74cf5ac8dc94f0
snd-07 <rect20250725@rect20250725noMacBook-Air.local>
2026-03-03 12:21:51 +0900

    kvのレスポンシブ対応

--- src/app/demand/ad/KeyVisual.tsx
+++ src/app/demand/ad/KeyVisual.tsx
@@ -8,7 +8,7 @@ import image from '../../../../public/image/demand/ad/advertise.json';
 const sxStyles = {
   imageBox: {
     maxWidth: '414px',
-    width: 'calc(100% - 485px)',
+    width: '100%',
     aspectRatio: '1/1',
     height: 'auto',
     overflow: 'hidden',
@@ -17,8 +17,12 @@ const sxStyles = {
     alignItems: 'center',
     justifyContent: 'center',
     position: 'relative',
+    gridRow: '1/4',
+    gridColumn: '2/3',
     '@media screen and (max-width:768px)': {
-      display: 'none',
+      gridRow: '3/4',
+      gridColumn: '1/2',
+      marginInline: 'auto',
     },
     '>div': {
       width: '200% !important',
@@ -28,22 +32,24 @@ const sxStyles = {
       top: '50%',
       left: '50%',
       transform: 'translate(-50%, -50%)',
-    }
+    },
   },
   subText: {
     '@media screen and (max-width:768px)': {
       textAlign: 'center',
-    }
-  }
-}
-
+    },
+  },
+};
 
 const KeyVisual = () => {
   return (
     <Box
       sx={{
-        pt: 0,
-        pb: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '414px',
       }}
     >
       <Box
@@ -52,81 +58,87 @@ const KeyVisual = () => {
           px: '20px',
           width: '100%',
           mx: 'auto',
-          display: 'flex',
+          display: 'grid',
+          gridTemplateColumns: '500px 1fr',
+          gridTemplateRows: '140px 68px 94px',
           alignItems: 'center',
           justifyContent: 'space-between',
           flexWrap: 'wrap',
-          rowGap: 3,
+          rowGap: 0,
           columnGap: 1,
-          minHeight: '414px',
           '@media screen and (max-width:768px)': {
-            flexDirection: 'column',
+            gridTemplateColumns: '1fr',
+            gridTemplateRows: 'auto',
             pt: 7,
-            pb: 3,
+            pb: 5,
           },
         }}
       >
+        <Typography
+          component="h2"
+          sx={{
+            fontSize: '50px',
+            letterSpacing: '0.04em',
+            lineHeight: '1.4',
+            fontWeight: 'bold',
+            gridRow: '1/2',
+            gridColumn: '1/2',
+            '@media screen and (max-width:768px)': {
+              fontSize: 'clamp(34px, 9.06vw, 50px)',
+              textAlign: 'center',
+            },
+          }}
+        >
+          クリックしたくなる
+          <br />
+          仕掛けを広告に。
+        </Typography>
         <Box
           sx={{
-            '@media screen and (max-width:460px)': {
-              textAlign: 'center',
-              width: '100%',
+            mt: 2,
+            gridRow: '2/3',
+            gridColumn: '1/2',
+            '@media screen and (max-width:768px)': {
+              gridRow: '2/3',
+              gridColumn: '1/2',
             },
           }}
         >
           <Typography
-            component="h2"
-            sx={{
-              fontSize: '50px',
-              letterSpacing: '0.04em',
-              lineHeight: '1.4',
-              fontWeight: 'bold',
-              '@media screen and (max-width:768px)': {
-                fontSize: 'clamp(34px, 9.06vw, 50px)',
-                textAlign: 'center',
-              },
-            }}
+            sx={[sxStyles.subText, commonSxStyles.typography.largeText]}
           >
-            クリックしたくなる
+            複雑な企画やデザインもプロにお任せ。
             <br />
-            仕掛けを広告に。
+            今すぐ使えるインタラクティブ広告を制作します。
           </Typography>
-          <Box
-            sx={{
-              mt: 2,
-            }}
-          >
-            <Typography sx={[sxStyles.subText,commonSxStyles.typography.largeText]}>
-              複雑な企画やデザインもプロにお任せ。
-              <br />
-              今すぐ使えるインタラクティブ広告を制作します。
-            </Typography>
-          </Box>
-          <Box
-            mt={4}
-            sx={{
-              width: '100%',
-              '& > a': {
-                '@media screen and (max-width:768px)': {
-                  width: '100%',
-                },
+        </Box>
+        <Box
+          mt={4}
+          sx={{
+            width: '100%',
+            gridRow: '3/4',
+            gridColumn: '1/2',
+            '@media screen and (max-width:768px)': {
+              gridRow: '4/5',
+              gridColumn: '1/2',
+            },
+            '& > a': {
+              '@media screen and (max-width:768px)': {
+                width: '100%',
               },
-            }}
-          >
-            <CustomButton
-              label="お問い合わせ"
-              icon="arrow-right"
-              href={contact}
-              isExternal={true}
-              gaLabel="demand_contact_form_keyvisual"
-            />
-          </Box>
+            },
+          }}
+        >
+          <CustomButton
+            label="お問い合わせ"
+            icon="arrow-right"
+            href={contact}
+            isExternal={true}
+            gaLabel="demand_contact_form_keyvisual"
+          />
         </Box>
         <Box sx={sxStyles.imageBox}>
-          <DotLottieReact
-            data={image}
-            autoplay
-          />
+          <DotLottieReact data={image} autoplay />
         </Box>
       </Box>
     </Box>
