87dda733a69141a6cb9992cf6695d2c57add7d22
snd-07 <rect20250725@rect20250725noMacBook-Air.local>
2026-05-22 20:26:12 +0900

    ファイル作成

--- src/app/career/for-business/page.tsx
+++ src/app/career/for-business/page.tsx
@@ -2,8 +2,7 @@ import '../../globals.css';
 
 export const metadata = {
   title: '【法人向け】Re:ProS Career',
-  description:
-    '',
+  description: '',
   robots: 'index,follow',
   alternates: {
     canonical: 'https://re-ct.co.jp/career/for-business',
