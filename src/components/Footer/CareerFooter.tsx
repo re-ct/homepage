f1a60d5d7a7d3afa841fdf879a610a7ffda23900
snd-07 <rect20250725@rect20250725noMacBook-Air.local>
2026-05-22 20:22:51 +0900

    フッターに採用サイトのリンク追加

--- src/components/Footer/CareerFooter.tsx
+++ src/components/Footer/CareerFooter.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { CommonLink } from '../CommonLink';
-import { Box, Typography } from '@mui/material';
+import { Box, List, ListItem, Typography, ListItemText } from '@mui/material';
 import { contact } from '../../lib/const/Link';
+import logo from '../../../public/image/career/logo_repros_career.svg';
+import Image from 'next/image';
 
 const sxStyles = {
   copy: {
@@ -55,54 +57,122 @@ const sxStyles = {
       },
     },
   },
+  footer: {
+    paddingTop: '40px',
+    borderTop: 'solid 1px #E7E7E7',
+  },
+  footerWrap: {
+    maxWidth: '932px',
+    px: '16px',
+    mx: 'auto',
+    paddingBottom: '60px',
+  },
+  title: {
+    letterSpacing: '1px',
+    fontSize: '14px',
+    marginBottom: '12px',
+  },
+  links: {
+    marginTop: '36px',
+  },
+  linksItem: {
+    padding: 0,
+    letterSpacing: '1px',
+    fontSize: '12px',
+    flexDirection: 'row',
+    '&::before': {
+      content: '"・"',
+    },
+    '& + &': {
+      marginTop: '6px',
+    },
+    '& > a': {
+      transition: 'all 0.3s linear',
+      '&:hover': {
+        textDecoration: 'underline',
+      },
+    },
+  },
 };
 
 const CareerFooter = () => {
+  const footerContent = [
+    { text: '求職者の方', href: '/career' },
+    { text: '採用担当者の方', href: '/career/for-business' },
+    {
+      text: '求職者・求人者の皆様へのご案内',
+      href: '/career/regulations/',
+    },
+  ];
   return (
-    <Box component="footer" sx={sxStyles.copy}>
-      <Box sx={sxStyles.copyWrap}>
-        <Typography sx={sxStyles.copyText}>©RECT 2025</Typography>
-        <Box sx={sxStyles.list}>
-          <CommonLink
-            link={{
-              href: '/career/regulations/',
-              external: false,
-            }}
-          >
-            求職者・求人者の皆様へのご案内
-          </CommonLink>
-          <CommonLink
-            link={{
-              href: '/',
-              external: false,
-            }}
-          >
-            運営会社
-          </CommonLink>
-          <CommonLink
-            link={{
-              href: contact,
-              external: true,
-            }}
-          >
-            お問い合わせ
-          </CommonLink>
-          <CommonLink
-            link={{
-              href: '/privacy/',
-              external: false,
-            }}
-          >
-            プライバシーポリシー
-          </CommonLink>
-          <CommonLink
-            link={{
-              href: '/announcement/',
-              external: false,
-            }}
-          >
-            電子公告
-          </CommonLink>
+    <Box component="footer" sx={sxStyles.footer}>
+      <Box sx={sxStyles.footerWrap}>
+        <Typography sx={sxStyles.title}>
+          エンジニアのキャリアに関するお悩みをサポート
+        </Typography>
+        <Image
+          alt="エンジニアのお悩みを無料でサポート Re:ProS Career"
+          src={logo.src}
+          width={259}
+          height={33}
+          style={{ verticalAlign: 'bottom' }}
+        />
+        <List sx={sxStyles.links}>
+          {footerContent.map((link, index) => (
+            <ListItem key={index} sx={sxStyles.linksItem}>
+              {link.href ? (
+                <CommonLink
+                  link={{
+                    href: link.href,
+                    external: link.external || false,
+                  }}
+                >
+                  {link.text}
+                </CommonLink>
+              ) : (
+                <ListItemText primary={link.text} />
+              )}
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+      <Box component="footer" sx={sxStyles.copy}>
+        <Box sx={sxStyles.copyWrap}>
+          <Typography sx={sxStyles.copyText}>©RECT 2025</Typography>
+          <Box sx={sxStyles.list}>
+            <CommonLink
+              link={{
+                href: '/',
+                external: false,
+              }}
+            >
+              運営会社
+            </CommonLink>
+            <CommonLink
+              link={{
+                href: contact,
+                external: true,
+              }}
+            >
+              お問い合わせ
+            </CommonLink>
+            <CommonLink
+              link={{
+                href: '/privacy/',
+                external: false,
+              }}
+            >
+              プライバシーポリシー
+            </CommonLink>
+            <CommonLink
+              link={{
+                href: '/announcement/',
+                external: false,
+              }}
+            >
+              電子公告
+            </CommonLink>
+          </Box>
         </Box>
       </Box>
     </Box>
